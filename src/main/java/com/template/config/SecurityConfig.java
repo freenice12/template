4fc00df1d4887ab5bb8d7d2649ecc04572570952
jylee <freenice12@gmail.com>
2017-06-05 10:08:10 +0900

    add modified files

--- src/main/java/com/template/config/SecurityConfig.java
+++ src/main/java/com/template/config/SecurityConfig.java
@@ -1,18 +1,14 @@
 package com.template.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//@Configuration
+public class SecurityConfig {
 
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(final HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/*.json").access("isAnonymous()")
-		.and()
-		.authorizeRequests().anyRequest().authenticated();
-	}
+//	@Override
+//	protected void configure(final HttpSecurity http) throws Exception {
+//		http.authorizeRequests().antMatchers("/*.json").access("isAnonymous()")
+//		.and()
+//		.authorizeRequests().anyRequest().authenticated();
+//	}
 
 //	@Override
 //	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
